6d3e95bfd572a7a49122696abe05e8c9c362f368
Kiacoder <ai90maharat@gmail.com>
2025-11-11 12:34:23 +0330

    Update auth.py

--- Code/Python/auth.py
+++ Code/Python/auth.py
@@ -1,56 +1,62 @@
 "Decorators for route authentication and authorization"
 from functools import wraps
-from flask import request, session, flash, redirect, url_for, abort, current_app
+from flask import request, session, flash, redirect, url_for, abort
 from flask_wtf.csrf import CSRFError
+from .extensions import csrf_protector
+
 
 def login_required(decorated_route):
-    "Decorator to ensure that a user is logged in and not in data resolution process"
+    """Decorator to ensure that a user is logged in and not in the data resolution process."""
     @wraps(decorated_route)
     def decorated_function(*args, **kwargs):
         if "client_id" not in session:
-            flash("برای مشاهده این صفحه باید وارد شوید.", "Warning")
+            flash("برای مشاهده این صفحه باید وارد شوید.", "warning")
             return redirect(url_for("client.login_client", next=request.url))
-        if "client_idForResolution" in session:
-            flash("ابتدا باید اطلاعات حساب کاربری خود را تکمیل و اصلاح نمایید.", "Warning")
+
+        if "client_id_for_resolution" in session:
+            flash("ابتدا باید اطلاعات حساب کاربری خود را تکمیل و اصلاح نمایید.", "warning")
             return redirect(url_for("client.resolve_data_issues"))
+
         return decorated_route(*args, **kwargs)
     return decorated_function
 
 
 def resolution_required(f):
-    "Decorator to ensure that the user is in the data resolution process"
+    """Decorator to ensure that the user is in the data resolution process."""
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if "client_idForResolution" not in session:
-            flash("شما در وضعیت اصلاح اطلاعات قرار ندارید.", "Info")
+
+        if "client_id_for_resolution" not in session:
+            flash("شما در وضعیت اصلاح اطلاعات قرار ندارید.", "info")
             return redirect(url_for("client.login_client", next=request.url))
+
         return f(*args, **kwargs)
     return decorated_function
 
 
 def admin_required(decorated_route):
-    "Decorator to ensure that an admin is logged in"
+    """Decorator to ensure that an admin is logged in."""
     @wraps(decorated_route)
     def decorated_function(*args, **kwargs):
         if not session.get("AdminLoggedIn"):
             return redirect(url_for("admin.admin_login"))
+
         return decorated_route(*args, **kwargs)
     return decorated_function
 
 
 def admin_action_required(decorated_route):
-    "Decorator to ensure that an admin is logged in and CSRF protection is applied for admin actions"
+    """Decorator to ensure an admin is logged in and CSRF protection is applied."""
     @wraps(decorated_route)
     def decorated_function(*args, **kwargs):
         if not session.get("AdminLoggedIn"):
             return redirect(url_for("admin.admin_login"))
 
-        csrf_protect = current_app.extensions.get("csrf_protector")
-        if csrf_protect:
-            try:
-                csrf_protect.protect()
-            except CSRFError:
-                abort(400, "توکن امنیتی نامعتبر است (CSRF).")
+        try:
+
+            csrf_protector.protect()
+        except CSRFError:
+            abort(400, "توکن امنیتی نامعتبر است (CSRF).")
 
         return decorated_route(*args, **kwargs)
     return decorated_function
